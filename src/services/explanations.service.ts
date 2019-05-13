d37e190e5d89a20cf0749e198c11f992a6618554
Charles Justin Reusnow <Charles.J.Reusnow@centene.com>
2019-05-13 00:01:29 -0500

    Add README details
    Update `explanations.service.ts` to catch errors and display an error
    instead of just crashing.
    
    Ignoring the explanations json file, as it probably shouldn't be checked
    in.

--- src/services/explanations.service.ts
+++ src/services/explanations.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {map, switchMap, toArray} from "rxjs/operators";
+import {catchError, map, switchMap, toArray} from "rxjs/operators";
 import {Explanation, IExplanation} from "../models/explanation.model";
-import {from, Observable} from "rxjs";
+import {from, Observable, of} from "rxjs";
 import {plainToClass} from "class-transformer";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../environments/environment";
@@ -17,7 +17,11 @@ export class ExplanationsService {
     return this.http.get(`${environment.explanationsLocation}`).pipe(
       switchMap((iExplanations: Array<IExplanation>): Observable<IExplanation> => from(iExplanations)),
       map((iExplanation: IExplanation): Explanation => plainToClass(Explanation, iExplanation)),
-      toArray()
+      toArray(),
+      catchError((error: Error): Observable<Array<Explanation>> => {
+        console.error(error);
+        return of([]);
+      })
     );
   }
 }
