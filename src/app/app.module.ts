c7949f7d7ff50b90a67774c80095dbca7a6082fc
Charles Justin Reusnow <Charles.J.Reusnow@centene.com>
2019-05-14 17:35:05 -0500

    Apply some changes for gh-pages
    Add subtitle and proper page title.
    
    Remove app routing. Didn't really add value.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 
-import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ExplanationComponent} from "./explanation/explanation.component";
@@ -17,7 +16,6 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
     MatCardModule,
